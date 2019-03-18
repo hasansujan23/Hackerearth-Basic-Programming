4b723b8ff4e3293d1f99431368bd6891df3fe3ea
hasansujan23 <hasansujan23@gmail.com>
2019-03-18 23:48:32 +0600

    hackererth basic programming

---
+++ Life_the_Universe_and_Everything.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    while(1){
+        scanf("%d",&n);
+        if(n==42)
+            break;
+        else
+            printf("%d\n",n);
+    }
+    return 0;
+}
